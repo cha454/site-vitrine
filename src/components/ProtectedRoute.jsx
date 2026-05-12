2443a524dc4d6bd233a543ec00a9ac0ff7b19fa1
cha454 <securitnew@gmail.com>
2026-05-12 21:35:59 +0200

    fix: resolve build failure and cleanup CSS syntax errors

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,5 @@
 import { Navigate } from 'react-router-dom'
 import { auth } from '../firebase'
-import { useAuthState } from 'react-firebase-hooks/auth' // Need to install this or use standard hook
 
 // Since I didn't install react-firebase-hooks, I'll use a standard implementation
 import { useState, useEffect } from 'react'
